01899a1a9af651b59084ce087b87ee524ea983df
Dammydrizzle <Dammydrizzle@gmail.com>
2024-01-18 17:30:04 +0100

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Heading from './Heading';
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   const [word, setWord] = React.useState('Eat')
+   function handleClick() {
+      setWord('Drink')
+   }
+
+
+   return (
+    <div>
+    <Heading message ={word + ' at Little Lemon'}/>
+    <button onClick={handleClick}>Click Here</button>
     </div>
-  );
+   )
 }
 
 export default App;
